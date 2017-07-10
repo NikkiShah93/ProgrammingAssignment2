593c7af5094b832d2e7147f107591a9db11c0c3b
Nikki Shah <neg.shahrokh@gmail.com>
2017-07-10 10:42:51 -0500

    Changes added

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+#We want to make two functions
+#One of them should do the followings:
+#1. Set the value of matrix
+#2. Get the value of matrix
+#3. Set the value of inversion
+#4. Get the value of inversion
 
-## Write a short comment describing this function
+#The second function should check if there is any value for that matrix or not
 
-makeCacheMatrix <- function(x = matrix()) {
+#First function: basically same as the example
 
+makeCacheMatrix <- function(matrixvalue = matrix()){
+    #checking to see if it is a matrix
+    #we need to set the inverse of matrix equal to NULL
+    matrixinversion <- NULL
+    #set value
+    set <- function(y){
+        matrixvalue <<- y
+        matrixinversion <<- NULL
+    }
+    
+    #get value
+    get <- function(){
+        matrixvalue
+    }
+    
+    #set inverse
+    setinversion <- function(inversevalue){
+        matrixinversion <<- inversevalue
+    }
+    
+    #get the inversion
+    getinversion <- function(){
+        matrixinversion
+    }
+    list(set = set, get = get, setinversion = setinversion, getinversion = getinversion)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#Second function:
+cacheSolve <- function(m,...){
+    
+    #using the getinversion function
+    matrixinversion <- m$getinversion()
+    #checking to see if it should be calculated or not
+    if(!is.null(matrixinversion)){
+        return(matrixinversion)
+    }
+    #Else:
+    #use the get for getting the matrix
+    specialmatrix <- m$get()
+    #using the matrix to get the inverse
+    matrixinversion <- solve(specialmatrix)
+    #saving the value
+    m$setinversion(matrixinversion)
+    #returning the value
+    return(matrixinversion)
+    
 }
